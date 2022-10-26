46eb647a234ccfff81079d75e2b3f0160cca1426
Tiago Pimentel <tiagomoreirapimentel@hotmail.com>
2022-10-26 16:45:59 -0300

    Update matriz_mostrar_valores_diaginais.java

--- Java_Primeiros_passos/matriz_mostrar_valores_diaginais/src/matriz_mostrar_valores_diaginais/matriz_mostrar_valores_diaginais.java
+++ Java_Primeiros_passos/matriz_mostrar_valores_diaginais/src/matriz_mostrar_valores_diaginais/matriz_mostrar_valores_diaginais.java
@@ -10,10 +10,13 @@ public class matriz_mostrar_valores_diaginais {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
+		// valor inserido pelo usuario para definir o tamanho da matriz e os valores adicionados
 		int N = sc.nextInt();
 
+		// define que a matriz terá o tamanho especificado pela variavel 'N'
 		int[][] mat = new int[N][N];
 
+		// adiciona os valores inseridos na matriz
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				mat[i][j] = sc.nextInt();
@@ -22,12 +25,14 @@ public class matriz_mostrar_valores_diaginais {
 
 		System.out.println("DIAGONAL PRINCIPAL:");
 
+		// percorre a matriz e mostra os valores armazenados na diagonal
 		for (int i = 0; i < N; i++) {
 			System.out.print(mat[i][i] + " ");
 		}
 
 		System.out.println();
 		
+		// percorre a matriz e conta quantos numeros armazenados são negativos e printa na tela
 		int cont = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
