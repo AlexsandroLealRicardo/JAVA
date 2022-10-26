9c0794a8f947861ec92a3b3217d3383816941f1c
Tiago Pimentel <tiagomoreirapimentel@hotmail.com>
2022-10-26 16:42:41 -0300

    Create matriz_mostrar_valores_diaginais.java

---
+++ Java_Primeiros_passos/matriz_mostrar_valores_diaginais/src/matriz_mostrar_valores_diaginais/matriz_mostrar_valores_diaginais.java
@@ -0,0 +1,45 @@
+package matriz_mostrar_valores_diaginais;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class matriz_mostrar_valores_diaginais {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		int[][] mat = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.println("DIAGONAL PRINCIPAL:");
+
+		for (int i = 0; i < N; i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+
+		System.out.println();
+		
+		int cont = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (mat[i][j] < 0) {
+					cont = cont + 1;
+				}
+			}
+		}
+		
+		System.out.println("QUANTIDADE DE NEGATIVOS = " + cont);
+		sc.close();
+
+	}
+
+}
