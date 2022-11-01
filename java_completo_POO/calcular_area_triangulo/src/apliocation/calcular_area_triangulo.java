c2e09abd47036d279e3de9d604e0378a9c5a2fa9
Tiago Pimentel <tiagomoreirapimentel@hotmail.com>
2022-11-01 18:17:38 -0300

    Update calcular_area_triangulo.java

--- java_completo_POO/calcular_area_triangulo/src/apliocation/calcular_area_triangulo.java
+++ java_completo_POO/calcular_area_triangulo/src/apliocation/calcular_area_triangulo.java
@@ -3,24 +3,33 @@ package apliocation;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entidade.triangulo;
+
 public class calcular_area_triangulo {
 
 	public static void main(String[] args) {
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		double xA, xB, xC, yA, yB, yC;
+		
+		triangulo x, y;
+		x = new triangulo();
+		y = new triangulo();
+	
 		System.out.println("Enter the measures of triangle X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		System.out.println("Enter the measures of triangle Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		double p = (x.a + x.b + x.c) / 2.0;
+		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+		p = (y.a + y.b + y.c) / 2.0;
+		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
 		if (areaX > areaY) {
