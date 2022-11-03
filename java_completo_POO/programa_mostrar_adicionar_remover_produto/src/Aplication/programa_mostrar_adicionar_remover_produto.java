d3ca6695049b10d4624661cf14efef8700a947eb
Tiago Pimentel <tiagomoreirapimentel@hotmail.com>
2022-11-03 11:32:18 -0300

    Update programa_mostrar_adicionar_remover_produto.java

--- java_completo_POO/programa_mostrar_adicionar_remover_produto/src/Aplication/programa_mostrar_adicionar_remover_produto.java
+++ java_completo_POO/programa_mostrar_adicionar_remover_produto/src/Aplication/programa_mostrar_adicionar_remover_produto.java
@@ -1,8 +1,8 @@
 package Aplication;
 
 import java.util.Locale;
-
 import java.util.Scanner;
+
 import Entities.Product;
 
 public class programa_mostrar_adicionar_remover_produto {
@@ -12,7 +12,7 @@ public class programa_mostrar_adicionar_remover_produto {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter product data: ");
 		System.out.println("Name: ");
-		String name = sc.next();
+		String name = sc.nextLine();
 		System.out.println("Price: ");
 		double price = sc.nextDouble();
 		System.out.println("Quantity in stock: ");
@@ -30,6 +30,7 @@ public class programa_mostrar_adicionar_remover_produto {
 		product.RemoveProducts(quantity);
 		System.out.printf("Product data: " + product);
 		System.out.println();
+		
 
 		sc.close();
 
